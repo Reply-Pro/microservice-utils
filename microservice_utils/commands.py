a1c7a7162786c9a0d8a28fa33cf3abc158ee1261
Martin R <23411718+RonquilloAeon@users.noreply.github.com>
2023-08-02 13:47:18 -0500

    add Command and CommandEnvelope functionality

--- microservice_utils/commands.py
+++ microservice_utils/commands.py
@@ -1,4 +1,7 @@
-from pydantic import BaseModel
+import json
+import time
+
+from pydantic import BaseModel, create_model
 
 
 class Command(BaseModel):
@@ -15,3 +18,33 @@ class CommandEnvelope(BaseModel):
     command: str
     parameters: Command
     timestamp: int
+
+    @classmethod
+    def create(cls, command: Command) -> "CommandEnvelope":
+        return cls(
+            command=command.name,
+            parameters=command,
+            timestamp=int(time.time()),
+        )
+
+    @classmethod
+    def from_published_json(
+        cls,
+        message: bytes,
+    ) -> "CommandEnvelope":
+        """Instantiate CommandEnvelope from a received message."""
+
+        json_msg = json.loads(message)
+
+        command = json_msg["command"]
+        parameters = json_msg["parameters"]
+        command_type = create_model(command, **parameters, __base__=Command)
+
+        return cls(
+            command=command,
+            parameters=command_type(**parameters),
+            timestamp=json_msg["timestamp"],
+        )
+
+    def to_publishable_json(self) -> bytes:
+        return self.json().encode("utf-8")
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -30,17 +30,18 @@ def test_command_envelope_to_publishable_json():
     assert command.json().encode("utf-8") in publishable
 
 
+@freeze_time("2023-08-02 19:10+00:00")
 def test_command_envelope_from_published_json():
     raw_message = b"""
-    {"command": "AddNewAccount", "parameters": {"title": "Walnut Marine", 
+    {"command": "AddNewAccount", "parameters": {"title": "Walnut Marine",
     "admin": {"email": "random@test.com"}}, "timestamp": 1691003400}
     """
 
     message = CommandEnvelope.from_published_json(raw_message)
 
-    assert message.command == "FakePaymentSubmitted"
+    assert message.command == "AddNewAccount"
     assert message.parameters == {
         "title": "Walnut Marine",
         "admin": {"email": "random@test.com"},
     }
-    assert message.timestamp == 1642620000
+    assert message.timestamp == 1691003400
